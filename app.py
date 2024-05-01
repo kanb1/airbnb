80551a9414475d9eca021d76f6bbb3a74685a6ac
Kanza - <kanza-@MacBook-Pro-tilhrende-Kanza.local>
2024-05-01 15:55:12 +0200

    pythonanywhere deploy test

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+#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
+
+#https://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@github.com/kanb1/airbnb.git
+
 #########################
 from bottle import default_app, get, post, run
 import git
@@ -14,7 +18,7 @@ def git_update():
 ##############################
 @get("/")
 def _():
-  return "One"
+  return "Two"
  
 ##############################
 try:
